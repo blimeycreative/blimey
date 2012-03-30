65d1a10ea1e70a2ede63f9db8372607e347d0804
Luke Rotherfield <luke@oxygenthinking.com>
2012-03-30 17:36:51 +0100

    contact theme extension bundle

--- src/Savvy/ContactExtensionBundle/Controller/ClientController.php
+++ src/Savvy/ContactExtensionBundle/Controller/ClientController.php
@@ -5,7 +5,9 @@ namespace Savvy\ContactExtensionBundle\Controller;
 use Oxygen\ContactBundle\Controller\ClientController as Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+/**
+ * @Route("/contact") 
+ */
 class ClientController extends Controller
 {
     

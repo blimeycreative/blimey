0b9f76e8b9b610b07254a613f72ab958ef29224f
Luke Rotherfield <luke@oxygenthinking.com>
2012-03-30 17:34:51 +0100

    contact theme extension bundle

--- src/Savvy/ContactExtensionBundle/SavvyContactExtensionBundle.php
+++ src/Savvy/ContactExtensionBundle/SavvyContactExtensionBundle.php
@@ -6,4 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SavvyContactExtensionBundle extends Bundle
 {
+    public function getParent(){
+        return 'OxygenContactBundle';
+    }
 }
